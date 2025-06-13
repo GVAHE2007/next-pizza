4f38b3d660e5fbb0b037c8884b85011b6f85d8d6
GVAHE2007 <vahegabrielyan2007adwwd@gmail.com>
2025-06-13 18:24:43 +0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { Header } from "@/components/header";
 import Image from "next/image";
 
 export default function Home() {
   return (
 
     <>
-      <h1>Barev</h1>
-      <Image src={"/next.svg"} alt={""} width={100} height={100} />
+      <Header title={""} />
     </>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+
+interface Props {
+    className?: string;
+    title: string;
+
+}
+
+export const Header: React.FC<Props> = (props) => {
+    const { className } = props;
+    return (
+        <header className={cn("", className)}>
+            <Button>
+                Barev
+            </Button>
+        </header>
+    );
+}
